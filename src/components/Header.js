e0669da729f8cc385a6cfb53a50959c5b666ecd2
Joseph Pereira <joseph.carlosx@gmail.com>
2023-06-15 16:34:47 -0400

    feat: Adicionado Header, ainda não adicionado ao Props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
   render() {
+    const { user } = this.props;
     return (
-      <div>Header</div>
+      <div>
+        <header>
+          <p data-testid="email-field">
+            {user.email}
+          </p>
+          <p data-testid="total-field">
+            0
+          </p>
+          <p data-testid="header-currency-field">
+            BRL
+          </p>
+        </header>
+      </div>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => {
+  console.log(state);
+  return {
+    user: state.user,
+    wallet: state.wallet,
+  };
+};
+
+export default connect(mapStateToProps)(Header);
