315cdc9b2894cd7b4af1283c25b635b4e2653421
Joseph Pereira <joseph.carlosx@gmail.com>
2023-06-19 17:01:50 -0400

    ainda falta fazer proptype requisito 6

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,24 +1,59 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Table extends Component {
   render() {
+    const { wallet: { expenses } } = this.props;
     return (
       <table>
         <thead>
-          <th />
-          <th>Descrição;</th>
-          <th>Tag;</th>
-          <th>Método de pagamento;</th>
-          <th>Valor;</th>
-          <th>Moeda;</th>
-          <th>Câmbio utilizado;</th>
-          <th>Valor convertido;</th>
-          <th>Moeda de conversão;</th>
-          <th>Editar/Excluir.</th>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
         </thead>
+        <tbody>
+          {
+            expenses.map((expense) => {
+              const { id,
+                description,
+                tag,
+                method,
+                value,
+                currency,
+                exchangeRates } = expense;
+              const valueOfNumber = (Number(value)).toFixed(2);
+              const usedRate = Number(exchangeRates[currency].ask);
+              const convertValue = (usedRate * valueOfNumber).toFixed(2);
+
+              return (
+                <tr key={ id }>
+                  <td>{description}</td>
+                  <td>{tag}</td>
+                  <td>{method}</td>
+                  <td>{valueOfNumber}</td>
+                  <td>{currency}</td>
+                  <td>{usedRate}</td>
+                  <td>{convertValue}</td>
+                  <td>Real</td>
+                  <td>Editar/Excluir</td>
+                </tr>
+              );
+            })
+          }
+        </tbody>
       </table>
     );
   }
 }
+const mapStateToProps = (state) => ({
+  user: state.user,
+  wallet: state.wallet,
+});
 
-export default Table;
+export default connect(mapStateToProps)(Table);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
 import WalletForm from '../components/WalletForm';
+import Table from '../components/Table';
 
 class Wallet extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class Wallet extends React.Component {
         TrybeWallet
         <Header />
         <WalletForm />
+        <Table />
       </div>
     );
   }
