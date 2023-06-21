6d66dd9fb0bd6854ec502f4d2971bd990a56dbe1
Joseph Pereira <joseph.carlosx@gmail.com>
2023-06-21 17:39:18 -0400

    teste assincrono não passando

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -2,46 +2,58 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { act } from 'react-dom/test-utils';
 import App from '../App';
-import mockData from './helpers/mockData';
 import { renderWithRouterAndRedux } from './helpers/renderWith';
 
 describe('Testa o Componente <Login/>', () => {
-  renderWithRouterAndRedux(<App />);
-  const inputEmail = screen.getByTestId('email-input');
-  const inputPassword = screen.getByTestId('password-input');
-  const { history } = renderWithRouterAndRedux(<App />);
-
   it('Verifica se o formulário está sendo renderizado na tela', () => {
-    // const { history } = renderWithRouterAndRedux(<App />);
+    const { history } = renderWithRouterAndRedux(<App />);
     const { location: { pathname } } = history;
     expect(pathname).toBe('/');
-    expect(inputEmail).toBeInTheDocument();
-    expect(inputPassword).toBeInTheDocument();
   });
-  it('Verifica se for passados emails com estrutura errada, o botão de entrar é habilitado', () => {
-    // renderWithRouterAndRedux(<App />);
-    // const inputEmail = screen.getByTestId('email-input');
-    const emailsToTest = ['teste@teste', 'teste.com'];
-
-    emailsToTest.forEach((test) => {
-      userEvent.type(inputEmail, test);
-      userEvent.type(inputPassword, '123456');
+  it(
+    `Verifica: 
+    - Formulário é renderizado na tela;
+    - Se forem passados emails com estrutura errada o botão "Entrar" é habilitado
+    - Se for passada uma senha menor que 6 dígitos, o botão de "Entrar" é habilitado`,
+    () => {
+      renderWithRouterAndRedux(<App />);
+      const inputEmail = screen.getByTestId('email-input');
+      const inputPassword = screen.getByTestId('password-input');
       const buttonLogin = screen.getByRole('button', { name: /entrar/i });
+      const emailsToTest = ['teste@teste', 'teste.com'];
+
+      expect(inputEmail).toBeInTheDocument();
+      expect(inputPassword).toBeInTheDocument();
+
+      emailsToTest.forEach((test) => {
+        userEvent.type(inputEmail, test);
+        userEvent.type(inputPassword, '12345');
+        expect(buttonLogin).toBeDisabled();
+        userEvent.clear(inputEmail);
+        userEvent.clear(inputPassword);
+      });
+
+      userEvent.type(inputEmail, 'teste@teste.com');
+      userEvent.type(inputPassword, '12345');
       expect(buttonLogin).toBeDisabled();
-    });
-  });
-  it('Verifica se uma senha menor do que 6 digitos for passada, o botão é habilitado', () => {
-    // renderWithRouterAndRedux(<App />);
-    // const inputEmail = screen.getByTestId('email-input');
-    // const inputPassword = screen.getByTestId('password-input');
-    // const buttonLogin = screen.getByRole('button', { name: /entrar/i });
+    },
+  );
+
+  it('Verifica path de redirecionamento', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+
+    const inputEmail = screen.getByTestId('email-input');
+    const inputPassword = screen.getByTestId('password-input');
+    const buttonLogin = screen.getByRole('button', { name: /entrar/i });
 
     userEvent.type(inputEmail, 'teste@teste.com');
-    userEvent.type(inputPassword, '12345');
-    const buttonLogin = screen.getByRole('button', { name: /entrar/i });
-    expect(buttonLogin).toBeDisabled();
-  });
-  it('Verifica path de redirecionamento', () => {
-    userEvent.type(inputEmail, 'teste@teste.com');
+    userEvent.type(inputPassword, '123456');
+    expect(buttonLogin).toBeEnabled();
+
+    act(() => {
+      userEvent.click(buttonLogin);
+    });
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/carteira');
   });
 });
